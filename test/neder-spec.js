3fe311c55b2ff0319087d2e607963321b14ae972
Gal Schlezinger <gal@spitfire.co.il>
2014-10-06 23:28:05 +0300

    Added simple specs. still doesn't have any support for the errors

--- test/neder-spec.js
+++ test/neder-spec.js
@@ -1,2 +1,61 @@
+var expect = require('chai').expect;
+var randomstring = require('randomstring').generate;
+
+var Neder = require('../lib/neder');
+
 describe("Neder", function() {
+  describe("#amen", function() {
+    it("should call the next function with a random number", function(done) {
+      var randomNumber = Math.floor(Math.random() * 1000);
+      Neder.amen(function(next) {
+        next(null, randomNumber);
+      }).then(function(data, next) {
+        expect(data).to.equal(randomNumber);
+        done();
+      });
+    });
+
+    it("should call the next function with a random string", function(done) {
+      var randomString = randomstring();
+      Neder.amen(function(next) {
+        next(null, randomString);
+      }).then(function(data, next) {
+        expect(data).to.equal(randomString);
+        done();
+      });
+    });
+
+    it("should call the next function with a deep object", function(done) {
+      var randomObject = {
+        someString: randomstring(),
+        someNumber: Math.random(),
+        someOtherObject: {
+          someString: randomstring(),
+          someNumber: Math.random()
+        }
+      };
+
+      Neder.amen(function(next) {
+        next(null, randomObject);
+      }).then(function(data, next) {
+        expect(data).to.deep.equal(randomObject);
+        done();
+      });
+    });
+
+    it("should support chaining", function(done) {
+      var randomString = randomstring();
+
+      Neder.amen(function(next) {
+        next(null, randomString);
+      }).then(function(data, next) {
+        expect(data).to.equal(randomString);
+        randomString = randomstring();
+        next(null, randomString);
+      }).then(function(data) {
+        expect(data).to.equal(randomString);
+        done();
+      });
+    });
+  })
 });
